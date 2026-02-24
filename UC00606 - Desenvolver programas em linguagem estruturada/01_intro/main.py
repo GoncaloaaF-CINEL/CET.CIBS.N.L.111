a5d7734d5a36fe4a1ad60b22497c2fbee933918d
GoncaloaaF <goncaloaaf@gmail.com>
2026-02-24 21:51:17 +0000

    demo 01_intro

--- UC00606 - Desenvolver programas em linguagem estruturada/01_intro/main.py
+++ UC00606 - Desenvolver programas em linguagem estruturada/01_intro/main.py
@@ -91,5 +91,13 @@ print(f"ola {nome}, tens {idade} anos e nascste em {ano_nasc}")
 """
 1 Faça um Programa que mostre a mensagem "ola mundo" na consola.
 2 Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número]. 
+--
+
+3 Faça um Programa que peça dois números e imprima a soma.
+4 Faça um Programa que peça as 4 notas bimestrais e mostre a média.
+--
+
+
+
 
 """
