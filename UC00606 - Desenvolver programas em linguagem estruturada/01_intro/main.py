df731a663e33a331d8b1e1486aaa9d0e4f2af6fd
GoncaloaaF <goncaloaaf@gmail.com>
2026-02-24 22:05:14 +0000

    ex 5 e 6

--- UC00606 - Desenvolver programas em linguagem estruturada/01_intro/main.py
+++ UC00606 - Desenvolver programas em linguagem estruturada/01_intro/main.py
@@ -90,14 +90,15 @@ print(f"ola {nome}, tens {idade} anos e nascste em {ano_nasc}")
 
 """
 1 Faça um Programa que mostre a mensagem "ola mundo" na consola.
-2 Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número]. 
+2 Faça um Programa que peça um número e então mostre a mensagem "O número informado foi [número]". 
 --
 
 3 Faça um Programa que peça dois números e imprima a soma.
 4 Faça um Programa que peça as 4 notas bimestrais e mostre a média.
 --
 
-
+5 Faça um Programa que converta metros para centímetros.
+6 Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
 
 
 """
